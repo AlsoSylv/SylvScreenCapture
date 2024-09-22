bfec1fadb0c2b70ace3c264bc07f9af2c199c50f
AlsoSylv <64978191+AlsoSylv@users.noreply.github.com>
2024-09-21 23:39:08 -0400

    Attempt #1 at DX9

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,19 @@ use interprocess::local_socket::{GenericNamespaced, Stream, ToNsName};
 use retour::RawDetour;
 use std::ffi::c_void;
 use std::io::{ErrorKind, Read, Write};
-use std::ptr::null_mut;
+use std::ptr::{null, null_mut};
 use std::sync::atomic::AtomicPtr;
 use std::sync::{Once, OnceLock};
-use windows::Win32::Foundation::HANDLE;
+use windows::Win32::Foundation::{HANDLE, RECT};
 use windows::Win32::Graphics::Direct3D11::{ID3D11Device1, D3D11_TEXTURE2D_DESC};
+use windows::Win32::Graphics::Direct3D9::{
+    D3D9b_SDK_VERSION, Direct3DCreate9, IDirect3DDevice9, IDirect3DDevice9Ex, IDirect3DTexture9,
+    D3DBACKBUFFER_TYPE_MONO, D3DCREATE_HARDWARE_VERTEXPROCESSING, D3DDEVTYPE_HAL, D3DFMT_A8R8G8B8,
+    D3DFMT_UNKNOWN, D3DFMT_X8R8G8B8, D3DMULTISAMPLE_NONE, D3DPOOL_DEFAULT, D3DPOOL_SYSTEMMEM,
+    D3DPRESENTFLAG_DEVICECLIP, D3DPRESENT_PARAMETERS, D3DSWAPEFFECT_COPY, D3DTEXF_NONE,
+    D3DUSAGE_DYNAMIC, D3DUSAGE_RENDERTARGET,
+};
+use windows::Win32::Graphics::Gdi::RGNDATA;
 use windows::Win32::System::SystemServices;
 use windows::{
     core::{s, Interface, HRESULT, PCSTR},
@@ -81,13 +89,28 @@ enum Reason {
     DllProcessDetach,
 }
 
+type Dx9PresentFunctionType = unsafe extern "system" fn(
+    *mut c_void,
+    *const RECT,
+    *const RECT,
+    HWND,
+    *const RGNDATA,
+) -> HRESULT;
+
 type PresentFunctionType = unsafe extern "system" fn(*mut c_void, u32, DXGI_PRESENT) -> HRESULT;
+
+union PresentFunctions {
+    dx9: Dx9PresentFunctionType,
+    dx11: PresentFunctionType,
+}
 
 static DETOUR: OnceLock<RawDetour> = OnceLock::new();
 
 static SHARED_BUFFER: OnceLock<ID3D11Texture2D> = OnceLock::new();
 
-static TRAMPOLINE: OnceLock<PresentFunctionType> = OnceLock::new();
+static DX9_SHARED_BUFFER: AtomicPtr<c_void> = AtomicPtr::new(null_mut());
+
+static TRAMPOLINE: OnceLock<PresentFunctions> = OnceLock::new();
 
 static SHARED_HANDLE: AtomicPtr<c_void> = AtomicPtr::new(null_mut());
 
@@ -123,7 +146,7 @@ fn main(hinst_dll: HINSTANCE, reason: Reason) -> Result<(), Error> {
         }
 
         std::thread::spawn(|| {
-            if let Err(e) = dll_attach() {
+            if let Err(e) = dll_attach_dx9() {
                 println!("{e}");
             }
         });
@@ -140,6 +163,131 @@ unsafe extern "system" fn def_window_pro_a(
     lparam: LPARAM,
 ) -> LRESULT {
     DefWindowProcA(hwnd, msg, wparam, lparam)
+}
+
+fn dll_attach_dx9() -> Result<(), Error> {
+    const WINDOW_CLASS_NAME: PCSTR = s!("dummy_window_for_swap_chain");
+
+    let window_class = WNDCLASSEXA {
+        cbSize: size_of::<WNDCLASSEXA>() as u32,
+        style: CS_HREDRAW | CS_VREDRAW,
+        lpfnWndProc: Some(def_window_pro_a),
+        hInstance: unsafe { GetModuleHandleA(None).unwrap().into() },
+        lpszClassName: WINDOW_CLASS_NAME,
+        cbClsExtra: 0,
+        cbWndExtra: 0,
+        hIcon: HICON::default(),
+        hCursor: HCURSOR::default(),
+        hIconSm: HICON::default(),
+        hbrBackground: HBRUSH::default(),
+        lpszMenuName: PCSTR::null(),
+    };
+
+    let registered_window_class = unsafe { RegisterClassExA(&window_class) };
+
+    if registered_window_class == 0 {
+        Err(windows::core::Error::from_win32())?
+    }
+
+    let window = unsafe {
+        CreateWindowExA(
+            WINDOW_EX_STYLE::default(),
+            WINDOW_CLASS_NAME,
+            WINDOW_CLASS_NAME,
+            WS_OVERLAPPEDWINDOW,
+            0,
+            0,
+            100,
+            100,
+            None,
+            None,
+            window_class.hInstance,
+            None,
+        )?
+    };
+
+    let d3d9 = unsafe { Direct3DCreate9(D3D9b_SDK_VERSION) };
+
+    let Some(d3d9) = d3d9 else {
+        std::process::exit(1);
+    };
+
+    let mut present_params = D3DPRESENT_PARAMETERS {
+        BackBufferWidth: 100,
+        BackBufferHeight: 100,
+        BackBufferFormat: D3DFMT_UNKNOWN,
+        BackBufferCount: 1,
+        MultiSampleType: D3DMULTISAMPLE_NONE,
+        MultiSampleQuality: 0,
+        SwapEffect: D3DSWAPEFFECT_COPY,
+        hDeviceWindow: window,
+        Windowed: true.into(),
+        EnableAutoDepthStencil: false.into(),
+        AutoDepthStencilFormat: D3DFMT_UNKNOWN,
+        Flags: D3DPRESENTFLAG_DEVICECLIP,
+        FullScreen_RefreshRateInHz: 0,
+        PresentationInterval: 1,
+    };
+
+    let mut device = None;
+
+    unsafe {
+        d3d9.CreateDevice(
+            0,
+            D3DDEVTYPE_HAL,
+            window,
+            D3DCREATE_HARDWARE_VERTEXPROCESSING as u32,
+            &mut present_params,
+            &mut device,
+        )?
+    };
+
+    let device = device.unwrap();
+
+    let present = device.vtable().Present;
+
+    unsafe {
+        DestroyWindow(window)?;
+        UnregisterClassA(WINDOW_CLASS_NAME, window_class.hInstance)?;
+    }
+
+    let detour = unsafe { RawDetour::new(present as _, new_dx9_present_function as _)? };
+
+    unsafe { detour.enable()? };
+
+    let tramponline: Dx9PresentFunctionType = unsafe { std::mem::transmute(detour.trampoline()) };
+
+    TRAMPOLINE.get_or_init(|| PresentFunctions { dx9: tramponline });
+    DETOUR.get_or_init(|| detour);
+
+    let name = r"\\.\pipe\sylvias_shared_handle.sock"
+        .to_ns_name::<GenericNamespaced>()
+        .unwrap();
+
+    let mut try_connect = Stream::connect(name.clone());
+
+    let mut stream = loop {
+        match try_connect {
+            Err(e) if e.kind() == ErrorKind::NotFound => {
+                try_connect = Stream::connect(name.clone());
+            }
+            Err(e) => {
+                println!("{e}");
+                try_connect = Stream::connect(name.clone());
+            }
+            Ok(stream) => {
+                break stream;
+            }
+        }
+    };
+
+    stream.write(&std::process::id().to_le_bytes()).unwrap();
+    let mut handle = [0; 8];
+    stream.read_exact(&mut handle).unwrap();
+    let ptr = isize::from_le_bytes(handle);
+    SHARED_HANDLE.store(ptr as _, std::sync::atomic::Ordering::Relaxed);
+
+    Ok(())
 }
 
 fn dll_attach() -> Result<(), Error> {
@@ -240,7 +388,7 @@ fn dll_attach() -> Result<(), Error> {
 
     let tramponline: PresentFunctionType = unsafe { std::mem::transmute(detour.trampoline()) };
 
-    TRAMPOLINE.get_or_init(|| tramponline);
+    TRAMPOLINE.get_or_init(|| PresentFunctions { dx11: tramponline });
     DETOUR.get_or_init(|| detour);
 
     let name = r"\\.\pipe\sylvias_shared_handle.sock"
@@ -271,6 +419,63 @@ fn dll_attach() -> Result<(), Error> {
     SHARED_HANDLE.store(ptr as _, std::sync::atomic::Ordering::Relaxed);
 
     Ok(())
+}
+
+fn new_dx9_present_function(
+    this: *mut c_void,
+    src_rect: *const RECT,
+    dst_rect: *const RECT,
+    window: HWND,
+    rgn: *const RGNDATA,
+) -> HRESULT {
+    let this = unsafe { IDirect3DDevice9Ex::from_raw(this) };
+
+    let shared_ptr = DX9_SHARED_BUFFER.load(std::sync::atomic::Ordering::Relaxed);
+
+    if !shared_ptr.is_null() {
+        println!("You did it!!")
+    //         let surface = unsafe { this.GetBackBuffer(0, 0, D3DBACKBUFFER_TYPE_MONO).unwrap() };
+    //
+    //         let shared_tex = unsafe { IDirect3DTexture9::from_raw(shared_ptr) };
+    //         let shared_surface = unsafe { shared_tex.GetSurfaceLevel(0) }.unwrap();
+    //
+    //         unsafe { this.StretchRect(&surface, null(), &shared_surface, null(), D3DTEXF_NONE).unwrap() };
+    } else {
+        let handle = SHARED_HANDLE.load(std::sync::atomic::Ordering::Relaxed);
+
+        if !handle.is_null() && shared_ptr.is_null() {
+            let mut texture = None;
+
+            let maybe_shared_buffer = unsafe {
+                this.CreateTexture(
+                    1920,
+                    1080,
+                    1,
+                    D3DUSAGE_DYNAMIC as u32,
+                    D3DFMT_A8R8G8B8,
+                    D3DPOOL_SYSTEMMEM,
+                    &mut texture,
+                    &mut HANDLE(handle),
+                )
+            };
+
+            match texture {
+                Some(shared_buffer) => {
+                    DX9_SHARED_BUFFER
+                        .store(shared_buffer.as_raw(), std::sync::atomic::Ordering::Relaxed);
+                }
+                None => {
+                    println!("Error opening shared texture {maybe_shared_buffer:?}")
+                }
+            }
+        }
+    }
+
+    let present_function = TRAMPOLINE
+        .get()
+        .expect("The trampoline was set before this was ever called.");
+
+    unsafe { (present_function.dx9)(this.as_raw(), src_rect, dst_rect, window, rgn) }
 }
 
 fn new_present_function(this: *mut c_void, sync_internal: u32, flags: DXGI_PRESENT) -> HRESULT {
@@ -317,5 +522,5 @@ fn new_present_function(this: *mut c_void, sync_internal: u32, flags: DXGI_PRESE
         .get()
         .expect("The trampoline was set before this was ever called.");
 
-    unsafe { present_function(this.as_raw(), sync_internal, flags) }
+    unsafe { (present_function.dx11)(this.as_raw(), sync_internal, flags) }
 }
