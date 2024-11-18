bc5c9d1171e799a9d368246c3182b65ddaeed7b7
AlsoSylv <64978191+AlsoSylv@users.noreply.github.com>
2024-11-18 16:00:09 -0500

    Hopefully functional VK support

--- screen-recorder/src/impls/vk.rs
+++ screen-recorder/src/impls/vk.rs
@@ -2,12 +2,18 @@ use crate::{RenderingAPI, SHARED_CPU_BUFFER};
 use retour::RawDetour;
 use std::ffi::c_char;
 use std::ptr::{null, null_mut};
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
 use std::sync::OnceLock;
 use vulkanalia::loader::LibloadingLoader;
-use vulkanalia::vk::{BufferCreateFlags, BufferUsageFlags, DeviceCommands, DeviceCreateInfo, DeviceMemory, DeviceV1_0, ExternalMemoryHandleTypeFlags, Fence, Handle, HasBuilder, Image, ImportMemoryWin32HandleInfoKHR, InstanceCreateInfo, InstanceV1_0, MemoryAllocateInfo, MemoryGetWin32HandleInfoKHR, PFN_vkAcquireNextImageKHR, PFN_vkCreateSwapchainKHR, PFN_vkQueuePresentKHR, PhysicalDeviceExternalBufferInfo, PhysicalDeviceExternalBufferInfoKHR, PresentInfoKHR, Queue, Result as VkResult, Semaphore, StructureType, SwapchainKHR};
+use vulkanalia::vk::{
+    CommandBuffer, DeviceCommands, DeviceCreateInfo, DeviceMemory, DeviceV1_0,
+    ExternalMemoryHandleTypeFlags, Fence, Handle, HasBuilder, Image, ImageLayout,
+    ImportMemoryWin32HandleInfoKHR, InstanceCreateInfo, InstanceV1_0, MemoryAllocateInfo,
+    PFN_vkAcquireNextImageKHR, PFN_vkCreateSwapchainKHR, PFN_vkQueuePresentKHR, PresentInfoKHR,
+    Queue, Result as VkResult, Semaphore, StructureType, SwapchainKHR,
+};
 use vulkanalia::{Device, Entry, Instance};
-use windows::Win32::Foundation::{HANDLE, HMODULE};
+use windows::Win32::Foundation::HMODULE;
 use windows::Win32::System::LibraryLoader::GetModuleFileNameA;
 
 static VK_PRESENT: OnceLock<<VkHooks as RenderingAPI>::PresentFn> = OnceLock::new();
@@ -77,7 +83,7 @@ impl RenderingAPI for VkHooks {
                 .unwrap()
         };
 
-        COMMANDS.get_or_init(|| device.commands().clone());
+        COMMANDS.get_or_init(|| *device.commands());
 
         let command = device.commands().acquire_next_image_khr;
 
@@ -117,6 +123,7 @@ impl RenderingAPI for VkHooks {
 }
 
 static DEVICE: AtomicUsize = AtomicUsize::new(0);
+static NEXT_IMAGE: AtomicU32 = AtomicU32::new(0);
 
 unsafe extern "system" fn vk_new_acquire_next_image(
     device: vulkanalia::vk::Device,
@@ -133,7 +140,10 @@ unsafe extern "system" fn vk_new_acquire_next_image(
     let acquire_next: PFN_vkAcquireNextImageKHR =
         unsafe { std::mem::transmute(NEXT_IMAGE_DETOUR.get().unwrap().trampoline()) };
 
-    unsafe { acquire_next(device, swapchain, timeout, semaphore, fence, image_index) }
+    let result = unsafe { acquire_next(device, swapchain, timeout, semaphore, fence, image_index) };
+    NEXT_IMAGE.store(*image_index, Ordering::SeqCst);
+
+    result
 }
 
 static COMMANDS: OnceLock<DeviceCommands> = OnceLock::new();
@@ -145,15 +155,27 @@ unsafe extern "system" fn vk_new_queue_present(
     let device = vulkanalia::vk::Device::from_raw(DEVICE.load(Ordering::SeqCst));
     let commands = COMMANDS.get().unwrap();
 
+    let info = unsafe { &*info };
+    let swapchain = unsafe { &*info.swapchains };
+
     if !device.is_null() {
         let header = SHARED_CPU_BUFFER.get().unwrap().0.header();
         if let Some(nt_handle) = header.get_nt_shared_handle() {
+            let mut shared_image = Image::null();
+
+            // TODO: Need a real image info struct
+            let result = (commands.create_image)(device, null(), null(), &mut shared_image);
+
+            if result != VkResult::SUCCESS {
+                println!("{result}")
+            }
+
             let result = (commands.allocate_memory)(device, null(), null(), null_mut());
 
             if result != VkResult::SUCCESS {
                 println!("{result}")
             }
-                
+
             let info = ImportMemoryWin32HandleInfoKHR {
                 s_type: StructureType::IMPORT_MEMORY_WIN32_HANDLE_INFO_KHR,
                 next: null(),
@@ -172,19 +194,56 @@ unsafe extern "system" fn vk_new_queue_present(
             let mut memory = DeviceMemory::null();
 
             let result = unsafe { (commands.allocate_memory)(device, &info, null(), &mut memory) };
-    
+
             if result != VkResult::SUCCESS {
                 println!("{result}")
             }
 
-            let image = Image::null(); // TODO: Make a real vulkan image
+            (commands.bind_image_memory)(device, shared_image, memory, 0);
 
-            (commands.bind_image_memory)(device, image, memory, 0);
-    
-            // TODO: copy buffer
+            let mut command_buffer = CommandBuffer::null();
+            (commands.allocate_command_buffers)(device, null(), &mut command_buffer); // TODO: Use proper info
+
+            (commands.begin_command_buffer)(command_buffer, null()); // TODO: Use proper info
+
+            let mut len = 0;
+            let mut images = Vec::new();
+            let result =
+                (commands.get_swapchain_images_khr)(device, *swapchain, &mut len, null_mut());
+            if result != VkResult::SUCCESS {
+                println!("{result}")
+            }
+            images.reserve(len as usize);
+
+            let result = (commands.get_swapchain_images_khr)(
+                device,
+                *swapchain,
+                &mut len,
+                images.as_mut_ptr(),
+            );
+
+            if result != VkResult::SUCCESS {
+                println!("{result}")
+            }
+
+            (commands.cmd_copy_image)(
+                command_buffer,
+                images[NEXT_IMAGE.load(Ordering::SeqCst) as usize],
+                ImageLayout::default(),
+                shared_image,
+                ImageLayout::default(),
+                0,
+                null(),
+            );
+
+            (commands.cmd_execute_commands)(command_buffer, 0, null());
+
+            let result = (commands.end_command_buffer)(command_buffer);
+
+            if result != VkResult::SUCCESS {
+                println!("{result}")
+            }
         }
-
-
     }
 
     VK_PRESENT.get().unwrap()(queue, info)
